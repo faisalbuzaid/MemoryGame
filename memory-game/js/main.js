f054559fd9b5026b062bad15f2c330cc4199294b
faisalbuzaid <36509591+faisalbuzaid@users.noreply.github.com>
2020-02-18 17:06:56 +0000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,11 +1,79 @@
 
-const cards = ["queen", "queen", "king", "king" ];
+const cards = [ 
+				{
+					rank: "queen",
+					suit: "hearts",
+					cardImage: "images/queen-of-hearts.png"
+				}, 
+				{
+					rank: "queen",
+					suit: "diamonds",
+					cardImage: "images/queen-of-diamonds.png"
+				}, 
+				{
+					rank: "king",
+					suit: "hearts",
+					cardImage: "images/king-of-hearts.png"
+				}, 
+				{
+					rank: "king",
+					suit: "diamonds",
+					cardImage: "images/king-of-diamonds.png"
+				}
+				];
 const cardsInPlay = [];
+
+let cardsLength = cards.length;
+
+/*	let cardOne = cardTow];
+	let cardTwo = cardTow];*/
+
+
+//	for(let i  0; i < cardsLength; i++){
+
+	//console.log(cards[i]);
+
+
+
+
+//}
+
+/*	for(let j in cards[i]){
+		console.log(j, cards[i], cards[i][j]);
+	}*/
+
+
+//console.log(cards[0]);
+
+/*for (let i = 0; i < cards.length; i++){
+	//console.log(cards[i].rank);
+	let childElement = cards[i].rank;
+	console.log(childElement); 
+
+}*/
+/////////////////////////////////////////////////////////////////////////////
 
 function flipCard(cardId){
 
 	let cardOne = cards[0];
 	let cardTwo = cards[2];
+
+	let cardOneRank = (cards[0].rank);
+	let cardOnImage = (cards[0].cardImage);
+	let cardOneSuit = (cards[0].suit);
+
+	let cardTowRank = (cards[1].rank);
+	let cardTowImage = (cards[1].cardImage);
+	let cardTowSuit = (cards[1].suit);
+
+	let cardThreeRank = (cards[2].rank);
+	let cardThreeImage = (cards[2].cardImage);
+	let cardThreeSuit = (cards[2].suit);
+
+	let cardFourRank = (cards[3].rank);
+	let cardFourImage = (cards[3].cardImage);
+	let cardFourSuit = (cards[3].suit);
+
 
 	//console.log(cardOne, cardTwo);
 
@@ -20,20 +88,27 @@ function flipCard(cardId){
 		let length2Confirmed = true;
 		//console.log(length2Confirmed);
 		if(cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("user flipped " + cardsInPlay[0]);
+			console.log("user flipped " + cardOneRank);
 			console.log("user flipped " + cardsInPlay[1]);
 			console.log("You Win!!");
 			alert("You find a match!");
 		} else{
-			console.log("user flipped " + cardsInPlay[0]);
-			console.log("user flipped " + cardsInPlay[1]);
+			console.log("user flipped " + cardOneRank);
+			console.log(cardOnImage);
+			console.log(cardOneSuit);
+
+			console.log("user flipped " + cardThreeRank);
+			console.log(cardThreeImage);
+			console.log(cardThreeSuit);
+	
+			//console.log("user flipped " + cardsInPlay[1]);
 			console.log("Sorry, try again.");
 			alert("Sorry, try again.");
 		}
 	}
 
 }
-
+///////////////////////////////////////////////////////////////////
 flipCard();
 
 //console.log("User flipped " + cardsInPlay);
