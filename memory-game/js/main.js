d9ff529bc9d7eb958c1206372b02347b3afdc92f
faisalbuzaid <36509591+faisalbuzaid@users.noreply.github.com>
2020-02-18 06:42:04 +0000

    Add files via upload

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+
+
+let cardOne = "queen";
+let cardTow = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
